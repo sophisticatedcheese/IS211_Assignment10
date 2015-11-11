28f61079b8ce8bac83f5a3acf2f302a65f6d8de2
sophisticatedcheese <sophisticatedcheese@gmail.com>
2015-11-10 19:10:49 -0500

    music.sql

---
+++ music.sql
@@ -0,0 +1,15 @@
+CREATE TABLE artists (ID INTEGER PRIMARY KEY
+					,Name TEXT)
+
+CREATE TABLE albums (ID INTEGER PRIMARY KEY
+					,Name TEXT
+					,ArtistID INTEGER)
+
+CREATE TABLE songs (ID INTEGER PRIMARY KEY
+					,Name TEXT
+					,ArtistID INTEGER
+					,AlbumID INTEGER
+					,TrackNumber INTEGER
+					,TrackLength INTEGER)
+
+#created tabled with columns.
